476bbe8e1e460f6370a2b737a74d12f464648c68
Ian Mayton <ian.mayton@gmail.com>
2020-07-14 21:59:44 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if index 
-  if board[index] == ''
-    return false
-  else
-    return true
-  end
+  if index.between?(0, 8)
+    if board[index] == ''
+      return false
+    else
+      return true
+    end
 end
